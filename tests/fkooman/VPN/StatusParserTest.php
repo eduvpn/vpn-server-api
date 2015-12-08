1edc91edd97fe544bd51e5adcf6ee5451e5b44a5
François Kooman <fkooman@tuxed.net>
2015-12-08 11:39:30 +0100

    add unit tests

---
+++ tests/fkooman/VPN/StatusParserTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace fkooman\VPN;
+
+use PHPUnit_Framework_TestCase;
+use fkooman\Json\Json;
+use fkooman\IO\IO;
+
+class StatusParserTest extends PHPUnit_Framework_TestCase
+{
+    public function testList()
+    {
+        $io = new IO();
+
+        $fileList = array(
+            __DIR__.'/data/no_clients.txt',
+            __DIR__.'/data/one_client.txt',
+#            __DIR__.'/data/three_clients.txt',
+        );
+
+        foreach ($fileList as $fileName) {
+            $outputFile = $fileName.'.output';
+
+            $statusParser = new StatusParser($io->readFile($fileName));
+
+            //var_dump(Json::encode($statusParser->getClientInfo()));
+
+            $this->assertSame(
+                Json::decodeFile($outputFile),
+                $statusParser->getClientInfo()
+            );
+        }
+    }
+}
