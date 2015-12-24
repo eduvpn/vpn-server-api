4d55e43284fbfe0e95c5f87d63179f544b36f925
François Kooman <fkooman@tuxed.net>
2015-12-24 13:39:35 +0100

    catch exception in ini-to-yaml

--- contrib/ini-to-yaml.php
+++ contrib/ini-to-yaml.php
@@ -6,33 +6,36 @@ require_once '/usr/share/php/fkooman/Config/autoload.php';
 use fkooman\Config\YamlFile;
 use fkooman\Config\IniFile;
 
-// read old config file
-$iniFile = new IniFile('/etc/vpn-server-api/config.ini');
-$yamlFile = new YamlFile('/etc/vpn-server-api/config.yaml');
+try {
+    $iniFile = new IniFile('/etc/vpn-server-api/config.ini');
+    $yamlFile = new YamlFile('/etc/vpn-server-api/config.yaml');
 
-$iniConfig = $iniFile->readConfig();
-$newConfig = array();
+    $iniConfig = $iniFile->readConfig();
+    $newConfig = array();
 
-// OpenVpnManagement --> OpenVpn
-$newConfig['OpenVpn'] = array();
-$sockets = $iniConfig['OpenVpnManagement'];
+    // OpenVpnManagement --> OpenVpn
+    $newConfig['OpenVpn'] = array();
+    $sockets = $iniConfig['OpenVpnManagement'];
 
-for ($i = 0; $i < sizeof($sockets['socket']); ++$i) {
-    $newConfig['OpenVpn'][] = array(
-        'id' => sprintf('server_%s', $i),
-        'name' => sprintf('Server %s', $i),
-        'socket' => $sockets['socket'][$i],
-    );
+    for ($i = 0; $i < sizeof($sockets['socket']); ++$i) {
+        $newConfig['OpenVpn'][] = array(
+            'id' => sprintf('server_%s', $i),
+            'name' => sprintf('Server %s', $i),
+            'socket' => $sockets['socket'][$i],
+        );
+    }
+
+    // BasicAuthentication --> Users
+    $newConfig['Users'] = array();
+    $basicAuth = $iniConfig['BasicAuthentication'];
+    foreach ($basicAuth as $user => $hash) {
+        $newConfig['Users'][$user] = $hash;
+    }
+
+    // Crl --> Crl
+    $newConfig['Crl']['url'] = $iniConfig['Crl']['crlUrl'];
+    $newConfig['Crl']['path'] = $iniConfig['Crl']['crlPath'];
+    $yamlFile->writeConfig($newConfig);
+} catch (Exception $e) {
+    die(sprintf('ERROR: %s', $e->getMessage()).PHP_EOL);
 }
-
-// BasicAuthentication --> Users
-$newConfig['Users'] = array();
-$basicAuth = $iniConfig['BasicAuthentication'];
-foreach ($basicAuth as $user => $hash) {
-    $newConfig['Users'][$user] = $hash;
-}
-
-// Crl
-$newConfig['Crl']['url'] = $iniConfig['Crl']['crlUrl'];
-$newConfig['Crl']['path'] = $iniConfig['Crl']['crlPath'];
-$yamlFile->writeConfig($newConfig);
