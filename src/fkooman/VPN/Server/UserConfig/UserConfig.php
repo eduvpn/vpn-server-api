2a35b2dce3a2df81d9ac040db2a9c21cb587dfe8
François Kooman <fkooman@tuxed.net>
2016-04-25 23:32:41 +0200

    update declared type

--- src/fkooman/VPN/Server/UserConfig/UserConfig.php
+++ src/fkooman/VPN/Server/UserConfig/UserConfig.php
@@ -24,7 +24,7 @@ class UserConfig
     /** @var bool */
     private $disable;
 
-    /** @var string|false */
+    /** @var string|bool */
     private $otpSecret;
 
     public function __construct(array $configData)
