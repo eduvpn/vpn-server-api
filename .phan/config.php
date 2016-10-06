a60ee241f349144c855c291ffcb1e312dd1cab14
François Kooman <fkooman@tuxed.net>
2016-10-06 10:03:19 +0200

    add phan config

---
+++ .phan/config.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'directory_list' => [
+        'src',
+        'web',
+        'bin',
+        'vendor/christian-riesen/otp',
+        'vendor/christian-riesen/base32',
+        'vendor/eduvpn/common',
+        'vendor/guzzlehttp',
+        'vendor/psr/log',
+    ],
+    'exclude_analysis_directory_list' => [
+        'vendor/',
+    ],
+];
