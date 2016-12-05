deefbfb16d28d3564c1d689f77bcf6577585f962
François Kooman <fkooman@tuxed.net>
2016-12-05 14:36:31 +0100

    fix getLogEntry

--- src/Storage.php
+++ src/Storage.php
@@ -448,12 +448,22 @@ class Storage
     public function getLogEntry($dateTimeUnix, $ipAddress)
     {
         $stmt = $this->db->prepare(
-            'SELECT external_user_id, profile_id, common_name, ip4, ip6, connected_at, disconnected_at
-             FROM connection_log
+            'SELECT 
+                 external_user_id AS user_id, 
+                 profile_id, 
+                 common_name, 
+                 ip4, 
+                 ip6, 
+                 connected_at, 
+                 disconnected_at
+             FROM
+                 connection_log
              WHERE
                 (ip4 = :ip_address OR ip6 = :ip_address)
-                AND connected_at < :date_time_unix
-                AND (disconnected_at > :date_time_unix OR disconnected_at IS NULL)'
+             AND 
+                connected_at < :date_time_unix
+             AND 
+                (disconnected_at > :date_time_unix OR disconnected_at IS NULL)'
         );
         $stmt->bindValue(':ip_address', $ipAddress, PDO::PARAM_STR);
         $stmt->bindValue(':date_time_unix', $dateTimeUnix, PDO::PARAM_STR);
