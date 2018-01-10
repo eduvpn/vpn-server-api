a686b5314edd99a9d1118cbb8661eaa90a0dc225
François Kooman <fkooman@tuxed.net>
2018-01-10 16:50:06 +0100

    update (C)

--- tests/Api/data/openvpn_module_config.php
+++ tests/Api/data/openvpn_module_config.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * eduVPN - End-user friendly VPN.
+ *
+ * Copyright: 2016-2018, The Commons Conservancy eduVPN Programme
+ * SPDX-License-Identifier: AGPL-3.0+
+ */
+
 return  [
   'apiConsumers' => [
     'vpn-user-portal' => 'abcdef',
