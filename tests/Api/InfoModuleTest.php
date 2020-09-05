25b54733179aee5fdff58e596090a2a94b8d435d
François Kooman <fkooman@tuxed.net>
2020-09-05 10:37:29 +0200

    expose vpnCaKeyAlgo through "Info" API call

--- tests/Api/InfoModuleTest.php
+++ tests/Api/InfoModuleTest.php
@@ -92,6 +92,7 @@ class InfoModuleTest extends TestCase
             [
                 'valid_from' => '2020-05-07T13:51:31+00:00',
                 'valid_to' => '2025-05-07T13:56:31+00:00',
+                'ca_key_algo' => 'RSA',
             ],
             $this->makeRequest(
                 ['vpn-user-portal', 'aabbcc'],
