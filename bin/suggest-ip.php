fd26c942debf946c746d58531e1bd5e4eea7a1a0
François Kooman <fkooman@tuxed.net>
2020-09-26 18:50:49 +0200

    add --help to suggest-ip, add autoloader

--- bin/suggest-ip.php
+++ bin/suggest-ip.php
@@ -7,29 +7,34 @@
  * SPDX-License-Identifier: AGPL-3.0+
  */
 
+require_once dirname(__DIR__).'/vendor/autoload.php';
+
 /*
  * Generate example IPv4 and IPv6 address ranges for VPN clients.
  *
  * IPv4:
- * Random value for the second and third octet, e.g: 10.53.129.0/25
+ * Random value for the second and third octet, e.g: 10.53.129.0/24
  *
  * IPv6:
  * The IPv6 address is generated according to RFC 4193 (Global ID), it results
  * in a /64 network.
  */
 
-$showIpFour = true;
-$showIpSix = true;
-foreach ($argv as $arg) {
-    if ('-6' === $arg) {
-        $showIpFour = false;
+$showFamily = [4, 6];
+for ($i = 1; $i < $argc; ++$i) {
+    if ('-6' === $argv[$i]) {
+        $showFamily = [6];
     }
-    if ('-4' === $arg) {
-        $showIpSix = false;
+    if ('-4' === $argv[$i]) {
+        $showFamily = [4];
+    }
+    if ('--help' === $argv[$i]) {
+        echo 'SYNTAX: '.$argv[0].' [-4] [-6]'.PHP_EOL;
+        exit(0);
     }
 }
 
-if ($showIpFour) {
+if (in_array(4, $showFamily, true)) {
     $ipFourPrefix = sprintf(
     '10.%s.%s.0/24',
         hexdec(bin2hex(random_bytes(1))),
@@ -38,7 +43,7 @@ if ($showIpFour) {
     echo $ipFourPrefix.PHP_EOL;
 }
 
-if ($showIpSix) {
+if (in_array(6, $showFamily, true)) {
     $ipSixPrefix = sprintf(
         'fd%s:%s:%s:%s::/64',
         bin2hex(random_bytes(1)),
